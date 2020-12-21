c2b2b2596c0166cc01337446364ded9c5acca92b
Atajan <rahmedov.a007@gmail.com>
2020-12-21 11:48:29 +0300

    Lab345

---
+++ QuizApp/app/src/main/java/com/example/quizapp/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.quizapp;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private FirebaseUser currentUser;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
+
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        currentUser = mAuth.getCurrentUser();
+        if (currentUser == null) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        } else {
+            Intent intent = new Intent(this, TestActivity.class);
+            startActivity(intent);
+        }
+    }
+}
---
+++ QuizApp/app/src/main/java/com/example/quizapp/Test.java
@@ -0,0 +1,13 @@
+package com.example.quizapp;
+
+import java.io.Serializable;
+
+public class Test implements Serializable {
+    public String id;
+
+    public Test() {}
+
+    public Test(String id) {
+        this.id = id;
+    }
+}
